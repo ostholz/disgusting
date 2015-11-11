73e6310dc88e3d10e806f8316bd39515d686ba0e
Dong Wang <dw@i2dm.de>
2015-11-11 11:28:26 +0100

    a new secret key generated

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 DisgustingServer::Application.config.session_store :cookie_store, key: '_disgusting_server_session'
+DisgustingServer::Application.config.secret_key_base =  '329c9ac77030fcb3f4d5f60cdacba4fdbddf69344be678890fc5cf347702f58029251277e25f8c054ef751b34564f7638dd79b243348b205f28ef29f374cb9f1'
